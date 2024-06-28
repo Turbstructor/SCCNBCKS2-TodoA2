09328fd5c500c11050b0885271a379758d7b2a6d
Turbstructor <ycw0201@naver.com>
2024-06-28 16:50:22 +0900

    🎨 Update TaskService
    Previously all business-logic implementations were in `TaskService`, breaking overall layer dependency rules. This commit creates additional `TaskServiceImpl` that has all implementations from `TaskService` and solves the issue.

---
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/assignment/sccnbcks2todoa2/domain/task/service/TaskServiceImpl.kt
@@ -0,0 +1,80 @@
+package spartacodingclub.nbcamp.kotlinspring.assignment.sccnbcks2todoa2.domain.task.service
+
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import spartacodingclub.nbcamp.kotlinspring.assignment.sccnbcks2todoa2.domain.task.dto.request.CreateTaskRequest
+import spartacodingclub.nbcamp.kotlinspring.assignment.sccnbcks2todoa2.domain.task.dto.request.UpdateTaskRequest
+import spartacodingclub.nbcamp.kotlinspring.assignment.sccnbcks2todoa2.domain.task.dto.request.toEntity
+import spartacodingclub.nbcamp.kotlinspring.assignment.sccnbcks2todoa2.domain.task.dto.request.updateEntity
+import spartacodingclub.nbcamp.kotlinspring.assignment.sccnbcks2todoa2.domain.task.dto.response.TaskResponse
+import spartacodingclub.nbcamp.kotlinspring.assignment.sccnbcks2todoa2.domain.task.dto.response.from
+import spartacodingclub.nbcamp.kotlinspring.assignment.sccnbcks2todoa2.domain.task.repository.TaskRepository
+
+@Service
+class TaskServiceImpl (
+
+    private val taskRepository: TaskRepository
+) : TaskService {
+
+    override fun createTask(
+        request: CreateTaskRequest
+    ): TaskResponse =
+        TaskResponse.from(
+            taskRepository.save(
+                request.toEntity()
+            )
+        )
+
+
+    override fun readAllTasks(
+    ): List<TaskResponse> =
+        taskRepository.findAll().map {
+            TaskResponse.from(it)
+        }
+
+    override fun readTask(
+        taskId: Long
+    ): TaskResponse =
+        TaskResponse.from(
+            taskRepository.findByIdOrNull(taskId)
+                ?: throw RuntimeException("Task not found with id: $taskId")
+        )
+
+
+    override fun updateTaskData(
+        taskId: Long,
+        request: UpdateTaskRequest
+    ): TaskResponse {
+
+        val targetTask = taskRepository.findByIdOrNull(taskId)
+            ?: throw RuntimeException("Task not found with id: $taskId")
+
+        request.updateEntity(targetTask)
+
+        return TaskResponse.from(
+            taskRepository.save(targetTask)
+        )
+    }
+
+    override fun updateTaskCompletion(
+        taskId: Long
+    ): TaskResponse {
+
+        val targetTask = taskRepository.findByIdOrNull(taskId)
+            ?: throw RuntimeException("Task not found with id: $taskId")
+
+        targetTask.toggleTaskCompletion()
+
+        return TaskResponse.from(
+            taskRepository.save(targetTask)
+        )
+    }
+
+
+    override fun deleteTask(
+        taskId: Long
+    ) =
+        if (taskRepository.existsById(taskId)) taskRepository.deleteById(taskId)
+        else throw RuntimeException("Task not found with id: $taskId")
+
+}
